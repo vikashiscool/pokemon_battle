6be9e8d0a528f8b1923d38d34a3299936ae1450e
vikashiscool <vikash_parekh@yahoo.com>
2015-07-01 16:03:33 -0700

    Create pokemon battle app using Angular

--- base.js
+++ base.js
@@ -1,5 +1,17 @@
 var app = angular.module('pokemonApp', []);
 
 app.controller('PokemonCtrl', function($scope) {
+  $scope.pokemon = res.results.collection1; //refers to mock_data.js
 
+  $scope.iChooseYou = function(pokemon){
+    if (!$scope.image1){
+      $scope.image1 = pokemon.image.href;
+    } else {
+      $scope.image2 = pokemon.image.href;
+    }
+  }
+
+  $scope.fight = function(){
+    $scope.image2 = null;
+  }
 });
